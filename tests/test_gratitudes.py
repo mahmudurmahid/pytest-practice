76e2594773e6733565fb16c20b8eac94ad0bc6f3
mahmudur mahid <m.mahid@icloud.com>
2026-01-19 16:27:39 +0000

    test for gratitudes adding one string

--- tests/test_gratitudes.py
+++ tests/test_gratitudes.py
@@ -6,3 +6,11 @@ Initially, an empty list appears with opener
 def test_gratitude_for_empty_string():
     gratitudes = Gratitudes()
     assert gratitudes.format() == "Be grateful for: "
+
+"""
+Adding one string to return one gratitude
+"""
+def test_gratitute_adding_one_string():
+    gratitudes = Gratitudes()
+    gratitudes.add("your health")
+    assert gratitudes.format() == "Be grateful for: your health"
