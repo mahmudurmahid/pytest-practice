fbe0f07801d7e8b6d152f1e6094b1c8b71b1ab35
mahmudur mahid <m.mahid@icloud.com>
2026-01-19 15:52:56 +0000

    added tests for empty string, output of same string, size of string

--- tests/test_string_builder.py
+++ tests/test_string_builder.py
@@ -1,1 +1,26 @@
 from lib.string_builder import *
+
+"""
+Initially output is an empty string
+"""
+def test_string_builder_for_initial_empty_string():
+    string_builder = StringBuilder()
+    assert string_builder.output() == ""
+
+"""
+When a string is added, output is the same string
+"""
+def test_string_builder_to_return_same_string():
+    string_builder = StringBuilder()
+    string_builder.add("hello, world!")
+
+    assert string_builder.output() == "hello, world!"
+
+"""
+When a string is added, size of the string is returned
+"""
+def test_string_builder_to_return_size():
+    string_builder = StringBuilder()
+    string_builder.add("hello, world!")
+
+    assert string_builder.size() == 13
