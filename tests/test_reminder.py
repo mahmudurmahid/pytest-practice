beb5d2efba5fc79c026e7636d622aac6f0958d82
mahmudur mahid <m.mahid@icloud.com>
2026-01-19 19:02:21 +0000

    added test for reminder method

--- tests/test_reminder.py
+++ tests/test_reminder.py
@@ -1,4 +1,9 @@
+import pytest
 from lib.reminder import *
 
 def test_reminder():
-    pass
+    reminder = Reminder("Kay")
+    with pytest.raises(Exception) as e:
+        reminder.remind()
+    error_message = str(e.value)
+    assert error_message == "No reminder set!"
