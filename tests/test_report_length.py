4923a652305dcec645cbc9367290553395cdaa89
mahmudur mahid <m.mahid@icloud.com>
2026-01-19 14:32:24 +0000

    removed unecessary whitespace

--- tests/test_report_length.py
+++ tests/test_report_length.py
@@ -4,7 +4,6 @@ from lib.report_length import *
 If report length is correct,
 returns correct character length
 """
-
 def test_report_length_correct():
     result = report_length("hello, world")
     assert result == "This string was 12 characters long."
