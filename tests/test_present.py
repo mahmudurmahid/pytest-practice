9d018916699fe5c53d2abde12d24544d4b5001fe
mahmudur mahid <m.mahid@icloud.com>
2026-01-19 19:41:28 +0000

    added wrap and unwrap present test method

--- tests/test_present.py
+++ tests/test_present.py
@@ -1,6 +1,12 @@
 import pytest
 from lib.present import *
 
+"""Wrap and unwrap a present"""
+def test_present_wrap_and_unwrap():
+    present = Present()
+    present.wrap("football")
+    assert present.unwrap() == "football"
+
 """
 Wrapping a present twice should raise an exception
 """
