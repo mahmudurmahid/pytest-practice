c48e871ae2a50e826b6c013f009e9caf49bf25f0
mahmudur mahid <m.mahid@icloud.com>
2026-01-19 20:05:51 +0000

    added test_password_checker method for password less than 8 char

--- tests/test_password_checker.py
+++ tests/test_password_checker.py
@@ -1,3 +1,4 @@
+import pytest
 from lib.password_checker import *
 
 """
@@ -6,3 +7,13 @@ Password is equal or longer than 8 char
 def test_password_checker_8_char_long():
     password_checker = PasswordChecker()
     assert password_checker.check("12345678") == True
+
+"""
+Password is not 8 char
+"""
+def test_password_checker_less_than_8_char():
+    password_checker = PasswordChecker()
+    with pytest.raises(Exception) as e:
+        password_checker.check("1234567")
+    error_message = str(e.value)
+    assert error_message == "Invalid password, must be 8+ characters."
