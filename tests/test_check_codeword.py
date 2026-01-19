a88d858391a17db46ff46d267e7f18af4209ab61
mahmudur mahid <m.mahid@icloud.com>
2026-01-19 14:15:39 +0000

    added test_check_codeword file, and test for close result

--- tests/test_check_codeword.py
+++ tests/test_check_codeword.py
@@ -3,3 +3,7 @@ from lib.check_codeword import *
 def test_check_codeword_return_correct_come_in_for_horse():
     result = check_codeword("horse")
     assert result == "Correct! Come in."
+
+def test_check_codeword_return_close_but_nope_for_haste():
+    result = check_codeword("haste")
+    assert result == "Close, but nope."
