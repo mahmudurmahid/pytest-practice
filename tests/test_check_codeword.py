85b4807fe680f1d97aefbfa7c1f7e06735f2016e
mahmudur mahid <m.mahid@icloud.com>
2026-01-19 14:15:57 +0000

    added test_check_codeword file, and test for wrong result

--- tests/test_check_codeword.py
+++ tests/test_check_codeword.py
@@ -7,3 +7,7 @@ def test_check_codeword_return_correct_come_in_for_horse():
 def test_check_codeword_return_close_but_nope_for_haste():
     result = check_codeword("haste")
     assert result == "Close, but nope."
+
+def test_check_codeword_return_wrong_for_donkey():
+    result = check_codeword("monkey")
+    assert result == "WRONG!"
